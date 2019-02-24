5461388e91e1ce3d7645a7cf37a429c7de725ff1
Alessandro De Blasis <alex@deblasis.net>
2019-02-24 10:26:26 +0000

    bugfix: missing bits :)

--- examples/conditional-rendering/2.js
+++ examples/conditional-rendering/2.js
@@ -33,6 +33,39 @@ class ControlloLogin extends React.Component {
   }
 }
 
+function BenvenutoUtente(props) {
+  return <h1>Bentornato/a!</h1>;
+}
+
+function BenvenutoOspite(props) {
+  return <h1>Autenticati, per favore</h1>;
+}
+
+function Benvenuto(props) {
+  const utenteAutenticato = props.utenteAutenticato;
+  if (utenteAutenticato) {
+    return <BenvenutoUtente />;
+  }
+  return <BenvenutoOspite />;
+}
+
+
+function BottoneLogin(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function BottoneLogout(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
 ReactDOM.render(
   <ControlloLogin />,
   document.getElementById('root')
