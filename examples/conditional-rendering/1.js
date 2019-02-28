4035d87b4841fdad4d947e8d4574d5f185653b53
Alessandro De Blasis <alex@deblasis.net>
2019-02-28 05:38:44 +0000

    Translation of the page "Conditional rendering" (#110)
    * wip: conditional-rendering
    
    * Default codepen link description
    
    * Fixes "undefined" in codepen links
    
    When using a non-empty link title (translated) we get undefined
    Changing the default to the translated version and leaving them empty
    fixes the issue
    
    * conditional-rendering: done
    
    imported codepen examples into source code
    I'll ask if it's the case of doing that upstream as well for better...
    "translatability" 😂
    
    * bugfix: missing bits :)
    
    * prettier fixes
    
    * Highlighting offsets 🐛🔫
    
    * Update content/docs/conditional-rendering.md
    
    Co-Authored-By: deblasis <alex@deblasis.net>

---
+++ examples/conditional-rendering/1.js
@@ -0,0 +1,21 @@
+function BenvenutoUtente(props) {
+  return <h1>Bentornato/a!</h1>;
+}
+
+function BenvenutoOspite(props) {
+  return <h1>Autenticati, per favore</h1>;
+}
+
+function Benvenuto(props) {
+  const utenteAutenticato = props.utenteAutenticato;
+  if (utenteAutenticato) {
+    return <BenvenutoUtente />;
+  }
+  return <BenvenutoOspite />;
+}
+
+ReactDOM.render(
+  // Prova a cambiare in utenteAutenticato={true}:
+  <Benvenuto utenteAutenticato={false} />,
+  document.getElementById('root')
+);
---
+++ examples/conditional-rendering/4.js
@@ -0,0 +1,41 @@
+function MessaggioAvviso(props) {
+  if (!props.attenzione) {
+    return null;
+  }
+
+  return <div className="warning">Attenzione!</div>;
+}
+
+class Pagina extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {mostraAvviso: true};
+    this.handleToggleClick = this.handleToggleClick.bind(
+      this
+    );
+  }
+
+  handleToggleClick() {
+    this.setState(state => ({
+      mostraAvviso: !state.mostraAvviso,
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <MessaggioAvviso
+          attenzione={this.state.mostraAvviso}
+        />
+        <button onClick={this.handleToggleClick}>
+          {this.state.mostraAvviso ? 'Nascondi' : 'Mostra'}
+        </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Pagina />,
+  document.getElementById('root')
+);
