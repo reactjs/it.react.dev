4db42e3f121bd3c73fe989e5713b2e188df07588
Alessandro De Blasis <alex@deblasis.net>
2019-03-21 08:46:02 +0000

    DocSearch: apikey
    updating apiKey for the Italian website
    
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>

--- src/components/LayoutHeader/DocSearch.js
+++ src/components/LayoutHeader/DocSearch.js
@@ -22,8 +22,8 @@ class DocSearch extends Component<{}, State> {
     // eslint-disable-next-line no-undef
     if (window.docsearch) {
       window.docsearch({
-        apiKey: '36221914cce388c46d0420343e0bb32e',
-        indexName: 'react',
+        apiKey: '9b7d68e6dea8533a9582f0e8087e0b6d',
+        indexName: 'reactjs_it',
         inputSelector: '#algolia-doc-search',
       });
     } else {
